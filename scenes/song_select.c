b08194bdb868533ff584b204775c064c3fdfb2dc
羽月ひら <84394710+Hatsuki-Hira@users.noreply.github.com>
2026-06-06 22:04:54 +0800

    update

--- scenes/song_select.c
+++ scenes/song_select.c
@@ -2,3 +2,12 @@
 
 #include "../global.h"
 #include "../data/renderer.h"
+
+
+
+// 
+void update_song_select_ui0(void) {
+    display_text(user_config.width * 0.5 - 3, user_config.height * 0.5, "Tamithm", 107);
+    render(0);
+    SLEEP_MS(1000);
+}
